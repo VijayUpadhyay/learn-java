b1d78f67bae7c1c4e08aa2d739fbeb44b339af3e
Vijay Upadhyay <vijay.upadhyay92@gmail.com>
2022-12-25 19:38:57 +0530

    removed deprecated methods

--- core/src/main/java/org/vijay/core/CloneExample.java
+++ core/src/main/java/org/vijay/core/CloneExample.java
@@ -1,29 +1,34 @@
 package org.vijay.core;
 
-public class CloneExample implements Cloneable{
+import java.lang.reflect.InvocationTargetException;
 
-	String str = "vijay";
+public class CloneExample implements Cloneable {
+
 	public static void main(String[] args) {
-		
-		//1
+
+		// 1
 		CloneExample obj1 = new CloneExample();
-		
-		//2
+
+		// 2
 		try {
-			CloneExample obj2 = (CloneExample) Class.forName("com.vijay.core.CloneExample").newInstance();
+			CloneExample obj2 = (CloneExample) Class
+					.forName("org.vijay.core.CloneExample").getConstructors()[0]
+					.newInstance();
 			System.out.println(obj2.str);
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+		} catch (InstantiationException | IllegalAccessException
+				| ClassNotFoundException | IllegalArgumentException
+				| InvocationTargetException | SecurityException e) {
 			e.printStackTrace();
 		}
-		
-		//3
+
+		// 3
 		try {
 			CloneExample obj3 = (CloneExample) obj1.clone();
-			System.out.println("clone obj: "+obj3.str);
+			System.out.println("clone obj: " + obj3.str);
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
 	}
+	String str = "vijay";
 
 }
--- core/src/main/java/org/vijay/operators/IntegerValueCache.java
+++ core/src/main/java/org/vijay/operators/IntegerValueCache.java
@@ -4,16 +4,16 @@ public class IntegerValueCache {
 
 	public static void main(String[] args) {
 		Integer x = 400, y = 400;// same if x n y are in b/w -128 to 127
-        if (x == y)
-           System.out.println("Same");
-        else
-           System.out.println("Not Same");
+		if (x == y)
+			System.out.println("Same");
+		else
+			System.out.println("Not Same");
 
-        Integer x1 = new Integer(10), y1 = new Integer(10);
-        if (x1 == y1)
-           System.out.println("Same");
-        else
-           System.out.println("Not Same");
+		Integer x1 = Integer.valueOf(10), y1 = Integer.valueOf(10);
+		if (x1 == y1)
+			System.out.println("Same");
+		else
+			System.out.println("Not Same");
 	}
 
 }
